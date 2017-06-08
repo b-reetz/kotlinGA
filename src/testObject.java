d77e9a94bf4ef1c59d10ef8cd37fe77a1a2685b1
Brendan <brendan.reetz@gmail.com>
2017-06-08 17:04:35 +1200

    Added a few test files

---
+++ src/testObject.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Brendan on 6/8/17.
+ *
+ */
+public class testObject {
+
+    private double d1;
+    private double d2;
+
+
+    public testObject(double d, double b) {
+        d1 = d;
+        d2 = b;
+    }
+
+
+}
