1e4f47a49da9090ce03cf6d5905c48feb4238f0d
Brendan <brendan.reetz@gmail.com>
2017-06-09 13:21:16 +1200

    Fixed erros

--- src/RastriginTest.kt
+++ src/RastriginTest.kt
@@ -24,30 +24,29 @@ fun main(args: Array<String>) {
 
 }
 
-
-/**
- * The Rastrigin function applied to a double
- *
- * @param d the double to apply the rastrigin function to
- * @return the resulting value
- */
-private fun formula(d: Double): Double {
-    //((x[i] * x[i]) - (10 * Math.cos(2 * Math.PI * x[i])));
-    return (d * d) - (10 * Math.cos(2 * Math.PI * d))
-}
-
-/**
- * Calculates the fitness for a given member of the population using the Rastrigin function
- *
- * @param a the member of the population
- * @return the fitness for this member of the population
- */
-private fun fitness(a: Collection<Number>): Number {
-    val result: Double = 10.0 * a.size + a.sumByDouble { formula(it as Double) }
-    return result
-}
-
 private fun runBasicGA(population: Collection<Collection<Number>>) {
+
+    /**
+     * The Rastrigin function applied to a double
+     *
+     * @param d the double to apply the rastrigin function to
+     * @return the resulting value
+     */
+    fun formula(d: Double): Double {
+        //((x[i] * x[i]) - (10 * Math.cos(2 * Math.PI * x[i])));
+        return (d * d) - (10 * Math.cos(2 * Math.PI * d))
+    }
+
+    /**
+     * Calculates the fitness for a given member of the population using the Rastrigin function
+     *
+     * @param a the member of the population
+     * @return the fitness for this member of the population
+     */
+    fun fitness(a: Collection<Number>): Number {
+        val result: Double = 10.0 * a.size + a.sumByDouble { formula(it as Double) }
+        return result
+    }
 
     /**
      * The crossover function for this GA
@@ -92,13 +91,8 @@ private fun runBasicGA(population: Collection<Collection<Number>>) {
                 .mapTo(newSol) { (it as Double) + (rand.nextGaussian() * 0.001) }
         return newSol
     }
-<<<<<<< HEAD
-    GA(population, ::fitness, ::crossover, ::mutation, 4).run(5000, false)
-=======
-
     //runs the GA
-    GA(population, ::fitness, ::crossover, ::mutation, 4).run(500, false)
->>>>>>> e08f04dddfb10bdd91773f1c73f3ab80b9090303
+    GA(population, ::fitness, ::crossover, ::mutation).run(500, TOURNAMENT_SELECTION, 4, false)
 }
 
  
